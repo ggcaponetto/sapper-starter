3ecb6119bf8d8b5b1f475db29ec538eee929e934
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2020-10-21 12:34:06 +0200

    wip

---
+++ my-app/src/routes/index.svelte
@@ -0,0 +1,32 @@
+<script>
+    function handleMessage(event) {
+        alert(event.detail.text);
+    }
+
+    let Game;
+    setTimeout(() => {
+        import('./game.svelte').then(({default: gameModule}) => {
+            console.log("loaded game module", gameModule);
+            Game = gameModule;
+        });
+    }, 0)
+</script>
+
+<style>
+    :global(html, body, #sapper, main){
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+</style>
+{#if Game}
+    <Game on:message={handleMessage}/>
+{:else}
+    <div>loading...</div>
+{/if}
